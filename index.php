a43dc55e4b08dad065fd713a21a4623877339a54
Kyusar <e.hodee@orange.fr>
2021-02-08 18:49:15 +0100

    Premier router

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+$title = 'Accueil';
+
+require('controller/controller.php');
+try 
+{
+    if (isset($_GET['action']))
+    {
+        if ($_GET['action'] == 'bio')
+        {
+            require('view/bio.php');
+        }
+        elseif ($_GET['action'] == 'chapter')
+        {
+            require('view/chapter.php');
+        }
+        elseif ($_GET['action'] == "contact")
+        {
+            require('view/contact.php');
+        }
+        elseif ($_GET['action'] == "connexion")
+        {
+            require('view/connexion.php');
+        }
+        elseif ($_GET['action'] == "reading")
+        {
+            require('view/reading_page.php');
+        }
+    }
+    else
+    {
+        require('view/front_page_view.php');
+    }
+}
+catch(Exception $e)
+{
+    $errorMessage = $e->getMessage();
+    require('view/errorView.php');
+}
